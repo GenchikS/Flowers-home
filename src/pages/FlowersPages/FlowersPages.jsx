e56d29b94e744221506d2659376351b21328dad3
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-12-06 00:27:56 +0200

    v.1.1.5

--- src/pages/FlowersPages/FlowersPages.jsx
+++ src/pages/FlowersPages/FlowersPages.jsx
@@ -1,33 +1,45 @@
 import css from "./FlowersPages.module.css";
+import photo1 from "../../img/1.jpg";
+import photo2 from "../../img/2.jpg";
+import photo3 from "../../img/3.jpg";
+import photo4 from "../../img/4.jpg";
+import photo5 from "../../img/5.jpg";
+import photo6 from "../../img/6.jpg";
+import photo7 from "../../img/7.jpg";
+import photo8 from "../../img/8.jpg";
+import photo9 from "../../img/9.jpg";
+import photo10 from "../../img/10.jpg";
+import photo11 from "../../img/11.jpg";
+// import photo12 from "../../img/1.jpg";
+// import photo13 from "../../img/1.jpg";
+// import photo14 from "../../img/1.jpg";
+// import photo15 from "../../img/1.jpg";
+// import photo16 from "../../img/1.jpg";
+// import photo17 from "../../img/1.jpg";
+// import photo18 from "../../img/1.jpg";
+// import photo19 from "../../img/1.jpg";
+// import photo20 from "../../img/1.jpg";
+// import photo21 from "../../img/1.jpg";
+// import photo22 from "../../img/1.jpg";
+// import photo23 from "../../img/1.jpg";
+// import photo24 from "../../img/1.jpg";
+
 
 export default function FlowersPages() {
   return (
     <div className={css.containerTitle}>
       {/* <h1 className={css.title}>Flowers</h1> */}
-      <img src="../../../photo/1.jpg" alt="" />
-      <img src="../../../photo/2.jpg" alt="" />
-      <img src="../../../photo/3.jpg" alt="" />
-      <img src="../../../photo/4.jpg" alt="" />
-      <img src="../../../photo/5.jpg" alt="" />
-      <img src="../../../photo/6.jpg" alt="" />
-      <img src="../../../photo/7.jpg" alt="" />
-      <img src="../../../photo/8.jpg" alt="" />
-      <img src="../../../photo/9.jpg" alt="" />
-      <img src="../../../photo/10.jpg" alt="" />
-      <img src="../../../photo/11.jpg" alt="" />
-      <img src="../../../photo/12.jpg" alt="" />
-      <img src="../../../photo/13.jpg" alt="" />
-      <img src="../../../photo/14.jpg" alt="" />
-      <img src="../../../photo/15.jpg" alt="" />
-      <img src="../../../photo/16.jpg" alt="" />
-      <img src="../../../photo/17.jpg" alt="" />
-      <img src="../../../photo/18.jpg" alt="" />
-      <img src="../../../photo/19.jpg" alt="" />
-      <img src="../../../photo/20.jpg" alt="" />
-      <img src="../../../photo/21.jpg" alt="" />
-      <img src="../../../photo/22.jpg" alt="" />
-      <img src="../../../photo/23.jpg" alt="" />
-      <img src="../../../photo/24.jpg" alt="" />
+      <img src={photo1} alt="" />
+      <img src={photo2} alt="" />
+      <img src={photo3} alt="" />
+      <img src={photo4} alt="" />
+      <img src={photo5} alt="" />
+      <img src={photo6} alt="" />
+      <img src={photo7} alt="" />
+      <img src={photo8} alt="" />
+      <img src={photo9} alt="" />
+      <img src={photo10} alt="" />
+      <img src={photo11} alt="" />
     </div>
   );
 }
