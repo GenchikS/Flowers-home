7573451369c9f08bed6319d651c8c3c640f3e62c
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-12-05 23:42:52 +0200

    v.1.13

--- src/pages/FlowersPages/FlowersPages.jsx
+++ src/pages/FlowersPages/FlowersPages.jsx
@@ -3,7 +3,31 @@ import css from "./FlowersPages.module.css";
 export default function FlowersPages() {
   return (
     <div className={css.containerTitle}>
-      <h1 className={css.title}>Flowers</h1>
+      {/* <h1 className={css.title}>Flowers</h1> */}
+      <img src="../../../photo/1.jpg" alt="" />
+      <img src="../../../photo/2.jpg" alt="" />
+      <img src="../../../photo/3.jpg" alt="" />
+      <img src="../../../photo/4.jpg" alt="" />
+      <img src="../../../photo/5.jpg" alt="" />
+      <img src="../../../photo/6.jpg" alt="" />
+      <img src="../../../photo/7.jpg" alt="" />
+      <img src="../../../photo/8.jpg" alt="" />
+      <img src="../../../photo/9.jpg" alt="" />
+      <img src="../../../photo/10.jpg" alt="" />
+      <img src="../../../photo/11.jpg" alt="" />
+      <img src="../../../photo/12.jpg" alt="" />
+      <img src="../../../photo/13.jpg" alt="" />
+      <img src="../../../photo/14.jpg" alt="" />
+      <img src="../../../photo/15.jpg" alt="" />
+      <img src="../../../photo/16.jpg" alt="" />
+      <img src="../../../photo/17.jpg" alt="" />
+      <img src="../../../photo/18.jpg" alt="" />
+      <img src="../../../photo/19.jpg" alt="" />
+      <img src="../../../photo/20.jpg" alt="" />
+      <img src="../../../photo/21.jpg" alt="" />
+      <img src="../../../photo/22.jpg" alt="" />
+      <img src="../../../photo/23.jpg" alt="" />
+      <img src="../../../photo/24.jpg" alt="" />
     </div>
   );
 }
--- src/pages/HomePages/HomePages.jsx
+++ src/pages/HomePages/HomePages.jsx
@@ -3,7 +3,11 @@ import css from "./HomePages.module.css";
 export default function HomePages() {
   return (
     <div className={css.containerTitle}>
-        <h1 className={css.title}>Щастя розквітає зсередини</h1>
+      <h1 className={css.title}>Щастя розквітає зсередини</h1>
+      <p1 className={css.text}>
+        «Затишний Дім» - це в прямому сенсі будинок, невеликий, гарний, з
+        клумбами на терасі, декількома спальнями, санвузлом та кухнею.
+      </p1>
     </div>
-    );
+  );
 }
