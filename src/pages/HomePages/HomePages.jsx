9d32e117fd382a50372601a2ec8017c699397383
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-01-30 22:30:11 +0200

    corr

--- src/pages/HomePages/HomePages.jsx
+++ src/pages/HomePages/HomePages.jsx
@@ -4,10 +4,10 @@ export default function HomePages() {
   return (
     <div className={css.containerTitle}>
       <h1 className={css.title}>Щастя розквітає зсередини</h1>
-      <p1 className={css.text}>
+      <p className={css.text}>
         «Затишний Дім» - це в прямому сенсі будинок, невеликий, гарний, з
         клумбами на терасі, декількома спальнями, санвузлом та кухнею.
-      </p1>
+      </p>
     </div>
   );
 }
