0c6313a3d6c7610d6274585ed45ff65eafde906d
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-08-21 01:51:46 +0300

    +contact

--- src/pages/ContactsPages/ContactsPages.jsx
+++ src/pages/ContactsPages/ContactsPages.jsx
@@ -4,10 +4,11 @@ export default function FlowersPages() {
   return (
     <div className={css.containerTitle}>
       <h1 className={css.title}>Contacts:</h1>
-      <p className={css.textCity}>м. Суми,</p>
-      <p className={css.textNext}>вул. Вільного Козацтва буд. 3, кв. 4,</p>
-      <p className={css.textNext}>Городецька Тетяна</p>
-      <p className={css.textNext}>т. +380956658693</p>
+      <p className={css.textCity}>сел. Савинці,</p>
+      <p className={css.textNext}>вул. Івана Тихого буд. 58,</p>
+      {/* <p className={css.textNext}>Пазій Лариса</p> */}
+      <p className={css.textNext}>т. +380989979464</p>
+      <p className={css.textNext}>Відправляємо Новою Поштою</p>
     </div>
   );
 }
