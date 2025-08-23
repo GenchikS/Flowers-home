e9ea24bd382ead6de2b6510467c216e89a937a8c
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2025-08-23 20:00:13 +0300

    +css

--- src/pages/ContactsPages/ContactsPages.jsx
+++ src/pages/ContactsPages/ContactsPages.jsx
@@ -3,12 +3,19 @@ import css from "./ContactsPages.module.css";
 export default function FlowersPages() {
   return (
     <div className={css.containerTitle}>
-      <h2 className={css.title}>Контакти:</h2>
       <div className={css.textContoiner}>
+        <h2 className={css.title}>Контакти:</h2>
         <p className={css.textRegion}>Харківська обл., Ізюмський р-н.,</p>
         <p className={css.textCity}>сел. Савинці,</p>
         <p className={css.textStreet}>вул. Івана Тихого буд. 58,</p>
-        <p className={css.textTel}>т. +380989979464</p>
+        <ul className={css.icons}>
+          <li className={css.phoneTel}></li>
+          <li className={css.phoneTelegram}></li>
+          <li className={css.phoneViber}></li>
+          <li>
+            <p className={css.textTel}>+38 (098) 997 94 64</p>
+          </li>
+        </ul>
         <p className={css.textNp}>Відправляємо: Новою Поштою</p>
       </div>
     </div>
