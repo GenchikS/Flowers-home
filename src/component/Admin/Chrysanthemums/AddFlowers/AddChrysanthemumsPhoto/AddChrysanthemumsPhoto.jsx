d176ef09fa1cb6483d91796a6189ab82f838ab9e
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2026-06-19 01:29:46 +0300

    cssBackground

--- src/component/Admin/Chrysanthemums/AddFlowers/AddChrysanthemumsPhoto/AddChrysanthemumsPhoto.jsx
+++ src/component/Admin/Chrysanthemums/AddFlowers/AddChrysanthemumsPhoto/AddChrysanthemumsPhoto.jsx
@@ -89,7 +89,7 @@ export default function AddChrysanthemumsPhoto() {
                     //  className={styles.fileInput}
                     onChange={handleFileChangeWeb}
                   />
-                </div>,
+                </div>
               )}
           </div>
         )}
